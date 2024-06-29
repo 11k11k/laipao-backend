c8bb414f26c5d6041f8c2e65e2a41cc6ad8bc392
lyj <1041528020@qq.com>
2024-06-29 14:03:37 +0800

    整合easyExcel插件，测试读取excel数据

---
+++ src/main/java/com/laioj/project/once/ImportExcel.java
@@ -0,0 +1,35 @@
+package com.laioj.project.once;
+
+import cn.hutool.json.JSONUtil;
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.read.listener.PageReadListener;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+@Slf4j
+public class  ImportExcel {
+
+    public static void main(String[] args) {
+        // todo 记得改为自己的测试文件
+        String fileName = "D:\\springcloudTest\\laipao-backend-focus\\src\\main\\resources\\excel\\demo.xlsx";
+        readByListener(fileName);
+
+    }
+
+    // 写法1：JDK8+ ,不用额外写一个DemoDataListener
+    // since: 3.0.0-beta1
+    public static void readByListener(String fileName){
+
+        // 这里默认每次会读取100条数据 然后返回过来 直接调用使用数据就行
+        // 具体需要返回多少行可以在`PageReadListener`的构造函数设置
+        EasyExcel.read(fileName, XingQiuUserInfo.class,
+                new PageReadListener<XingQiuUserInfo>(dataList -> {
+                    for (XingQiuUserInfo demoData : dataList) {
+                        log.info("读取到一条数据{}", JSONUtil.toJsonStr(demoData));
+                    }
+                })).sheet().doRead();
+    }
+
+
+
+}
---
+++ src/main/java/com/laioj/project/once/XingQiuUserInfo.java
@@ -0,0 +1,19 @@
+package com.laioj.project.once;
+
+import com.alibaba.excel.annotation.ExcelProperty;
+import lombok.Data;
+
+@Data
+public class XingQiuUserInfo {
+    /**
+     * id
+     */
+    @ExcelProperty("成员编号")
+    private String planetCode;
+
+    /**
+     * 用户昵称
+     */
+    @ExcelProperty("成员昵称")
+    private String username;
+}
